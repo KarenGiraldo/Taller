25f22d65fedf24cd5b9c00bfd1d2efe9b004f91c
KarenGiraldo <karengiraldo.o@gmail.com>
2024-10-13 16:58:34 -0500

    modificacion

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config.suppress_callback_exceptions = True
 
 
 # Load data from csv
-#funcion de los datos
+#funcion de los datos...
 def load_data():
     df = pd.read_csv("datos_energia.csv")
     df['time'] = pd.to_datetime(df['time'])
